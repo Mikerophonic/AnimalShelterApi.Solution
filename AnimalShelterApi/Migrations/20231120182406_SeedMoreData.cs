f957eddce67644ae12729e7dca111bc19f4667bd
Mikerophonic <micgcon@gmail.com>
2023-11-20 10:27:27 -0800

    add more seed data for pagination

---
+++ AnimalShelterApi/Migrations/20231120182406_SeedMoreData.cs
@@ -0,0 +1,182 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AnimalShelterApi.Migrations
+{
+    public partial class SeedMoreData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 1,
+                columns: new[] { "Age", "Name", "Species" },
+                values: new object[] { 2, "Luna", "Cat" });
+
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 2,
+                columns: new[] { "Age", "Name", "Species" },
+                values: new object[] { 3, "Rocky", "Dog" });
+
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 3,
+                columns: new[] { "Age", "Name", "Species" },
+                values: new object[] { 1, "Sparkle", "Rabbit" });
+
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 4,
+                columns: new[] { "Age", "Name", "Species" },
+                values: new object[] { 5, "Ziggy", "Parrot" });
+
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 5,
+                columns: new[] { "Age", "Name" },
+                values: new object[] { 1, "Whiskers" });
+
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "AnimalId", "Age", "Name", "Species" },
+                values: new object[,]
+                {
+                    { 6, 4, "Shadow", "Snake" },
+                    { 7, 2, "Mochi", "Guinea Pig" },
+                    { 8, 2, "Tinker", "Ferret" },
+                    { 9, 1, "Nala", "Fish" },
+                    { 10, 1, "Blizzard", "Hedgehog" },
+                    { 11, 2, "Milo", "Dog" },
+                    { 12, 1, "Daisy", "Dog" },
+                    { 13, 3, "Simba", "Cat" },
+                    { 14, 4, "Cooper", "Dog" },
+                    { 15, 2, "Lily", "Cat" },
+                    { 16, 3, "Riley", "Dog" },
+                    { 17, 1, "Sasha", "Cat" },
+                    { 18, 2, "Teddy", "Dog" },
+                    { 19, 4, "Cleo", "Cat" },
+                    { 20, 1, "Bentley", "Dog" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 15);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 16);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 17);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 18);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 19);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 20);
+
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 1,
+                columns: new[] { "Age", "Name", "Species" },
+                values: new object[] { 7, "Matilda", "Dog" });
+
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 2,
+                columns: new[] { "Age", "Name", "Species" },
+                values: new object[] { 10, "Rexie", "Cat" });
+
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 3,
+                columns: new[] { "Age", "Name", "Species" },
+                values: new object[] { 2, "Matilda", "Dog" });
+
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 4,
+                columns: new[] { "Age", "Name", "Species" },
+                values: new object[] { 4, "Pip", "Cat" });
+
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 5,
+                columns: new[] { "Age", "Name" },
+                values: new object[] { 22, "Bartholomew" });
+        }
+    }
+}
--- AnimalShelterApi/Models/AnimalShelterApiContext.cs
+++ AnimalShelterApi/Models/AnimalShelterApiContext.cs
@@ -14,11 +14,26 @@ namespace AnimalShelterApi.Models
     {
       builder.Entity<Animal>()
         .HasData(
-          new Animal { AnimalId = 1, Name = "Matilda", Species = "Dog", Age = 7 },
-          new Animal { AnimalId = 2, Name = "Rexie", Species = "Cat", Age = 10 },
-          new Animal { AnimalId = 3, Name = "Matilda", Species = "Dog", Age = 2 },
-          new Animal { AnimalId = 4, Name = "Pip", Species = "Cat", Age = 4 },
-          new Animal { AnimalId = 5, Name = "Bartholomew", Species = "Hamster", Age = 22 }
+          new Animal { AnimalId = 1, Name = "Luna", Species = "Cat", Age = 2 },
+          new Animal { AnimalId = 2, Name = "Rocky", Species = "Dog", Age = 3 },
+          new Animal { AnimalId = 3, Name = "Sparkle", Species = "Rabbit", Age = 1 },
+          new Animal { AnimalId = 4, Name = "Ziggy", Species = "Parrot", Age = 5 },
+          new Animal { AnimalId = 5, Name = "Whiskers", Species = "Hamster", Age = 1 },
+          new Animal { AnimalId = 6, Name = "Shadow", Species = "Snake", Age = 4 },
+          new Animal { AnimalId = 7, Name = "Mochi", Species = "Guinea Pig", Age = 2 },
+          new Animal { AnimalId = 8, Name = "Tinker", Species = "Ferret", Age = 2 },
+          new Animal { AnimalId = 9, Name = "Nala", Species = "Fish", Age = 1 },
+          new Animal { AnimalId = 10, Name = "Blizzard", Species = "Hedgehog", Age = 1 },
+          new Animal { AnimalId = 11, Name = "Milo", Species = "Dog", Age = 2 },
+          new Animal { AnimalId = 12, Name = "Daisy", Species = "Dog", Age = 1 },
+          new Animal { AnimalId = 13, Name = "Simba", Species = "Cat", Age = 3 },
+          new Animal { AnimalId = 14, Name = "Cooper", Species = "Dog", Age = 4 },
+          new Animal { AnimalId = 15, Name = "Lily", Species = "Cat", Age = 2 },
+          new Animal { AnimalId = 16, Name = "Riley", Species = "Dog", Age = 3 },
+          new Animal { AnimalId = 17, Name = "Sasha", Species = "Cat", Age = 1 },
+          new Animal { AnimalId = 18, Name = "Teddy", Species = "Dog", Age = 2 },
+          new Animal { AnimalId = 19, Name = "Cleo", Species = "Cat", Age = 4 },
+          new Animal { AnimalId = 20, Name = "Bentley", Species = "Dog", Age = 1 }
         );
     }
   }
